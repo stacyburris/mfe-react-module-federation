5ddacd130a0c7db29d21afcf64971c3d04011c46
Stacy <stacy1burris@gmail.com>
2023-08-07 11:30:47 -0700

    bug

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -11,9 +11,9 @@ const prodConfig = {
     filename: '[name].[contenthash].js', // this tells webpack to use a unique name for our bundled js file
     publicPath: '/container/latest/', // this tells webpack where to put the bundled file when we run the build command 
   },
-  headers: {
-    'Access-Control-Allow-Origin': '*', // this allows us to make requests from any domain  
-  },
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*', // this allows us to make requests from any domain  
+  // },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container', // this name is used by the container to reference this module
